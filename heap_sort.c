4fea0ebe6834834a9f879a2898b2beaf912be497
adelmo.maximo <adnm.eng20@uea.edu.br>
2024-05-07 17:59:15 -0400

    add

--- heap_sort.c
+++ heap_sort.c
@@ -50,6 +50,11 @@ void heapSort(int arr[], int n) {
 int main() {
     int arr[] = {12, 11, 13, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Given array is \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
     heapSort(arr, n);
 
     printf("Sorted array is \n");
--- quick_sort.c
+++ quick_sort.c
@@ -39,7 +39,14 @@ void quickSort(int arr[], int low, int high) {
 int main() {
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Given array is \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
     quickSort(arr, 0, n - 1);
+    
     printf("Sorted array: \n");
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
